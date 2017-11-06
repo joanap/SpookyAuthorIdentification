1c3574bda8ce9693c50ea296db57fe55c8a2fb95
AlexGomesDS <Alexsantogomes@gmail.com>
2017-11-06 20:20:04 +0000

    [UPD] - Minor fixes in the import data code

--- Project/import_data.py
+++ Project/import_data.py
@@ -9,12 +9,14 @@ Created on Tue Oct 31 23:13:39 2017
 import numpy as np
 import pandas as pd
 import nltk
-
+import re
 import matplotlib.pyplot as plt
 
 #%% load data
 train_DataFrame = pd.read_csv('../input/train.csv')
 train_DataFrame.describe()
+
+stopwords = nltk.corpus.stopwords.words('english')
 
 #%% test with one example record
 example_record = train_DataFrame.loc[0]
@@ -29,7 +31,6 @@ vector_of_stopwords = [i for i in vector_of_words if (i in stopwords)]
 number_of_stop_words = len( vector_of_stopwords )
 
 #%% Create len feature for the whole dataset
-stopwords = nltk.corpus.stopwords.words('english')
 
 def count_stop_words(vector_of_words):
     vector_of_stopwords = [i for i in vector_of_words if (i.lower() in stopwords)] 
