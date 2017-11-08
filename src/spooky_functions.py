8409bb29a717fba397f53f32fd1774f0769592b7
AlexGomesDS <Alexsantogomes@gmail.com>
2017-11-08 22:22:06 +0000

    [UPD] - Added prediction with bigrams

---
+++ src/spooky_functions.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  8 22:09:43 2017
+
+@author: ASSG
+"""
+
+
+#%% Generic function to make predictions and output them to a valid csv file (só pq é pra ti Joana :P)
+
+def train_predict_and_export(X_train, X_test, Y_train, id, filename):
+    clf = MultinomialNB().fit(X_train, Y_train)
+    prediction = clf.predict_proba(X_test)
+    
+    classes = Y_train.unique()
+    classes.sort()
+    
+    submission_DF = pd.concat([id,
+                               pd.DataFrame(prediction, columns = classes)], 
+                        axis = 1)
+    
+    submission_DF.to_csv('../output/' + filename, index = False)
--- src/wordCounter.py
+++ src/wordCounter.py
@@ -20,7 +20,10 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
 
-#%# import data
+# our beautiful functions:
+from spooky_functions import train_predict_and_export
+
+#%% import data
 train_DataFrame = pd.read_excel('../input/parsed_sentences.xlsx')
 train_DataFrame.describe()
 
@@ -36,42 +39,46 @@ test_DataFrame = pd.read_csv('../input/test.csv')
 
 unigram_vect = CountVectorizer()
 
-bigram_vect = CountVectorizer(ngram_range=(2, 2), token_pattern=r'\b\w\w+\b', 
+Ngram_vect = CountVectorizer(ngram_range=(2, 2), token_pattern=r'\b\w\w+\b', 
                               min_df=2,
                               stop_words='english')
 
 
 #get word occurences per sentence
 unigram_counts = unigram_vect.fit_transform(train_DataFrame['text'])
-bigram_counts = bigram_vect.fit_transform(train_DataFrame['text'])
+Ngram_counts = Ngram_vect.fit_transform(train_DataFrame['text'])
 
 #get words
 unigram_features = unigram_vect.get_feature_names()
-bigram_features = bigram_vect.get_feature_names()
-print("Number of bigrams found {0}".format(len(bigram_features)))
+Ngram_features = Ngram_vect.get_feature_names()
+print("Number of Ngrams found {0}".format(len(Ngram_features)))
 
 unigram_counts_test = unigram_vect.transform(test_DataFrame['text'])
-
+Ngram_count_test = Ngram_vect.transform(test_DataFrame['text'])
 #%%
 #get the index by word
 unigram_vect.vocabulary_.get('document')
 
+#%% Apply term-frequency and inverse-document-frequency fixes
 tfidf_transformer = TfidfTransformer()
 unigrams_tfidf = tfidf_transformer.fit_transform(unigram_counts)
 unigrams_tfidf_test = tfidf_transformer.transform(unigram_counts_test)
+Ngrams_tfidf = tfidf_transformer.fit_transform(Ngram_counts)
+Ngrams_tfidf_test = tfidf_transformer.transform(Ngram_count_test)
 
-X_train = unigrams_tfidf
-X_test = unigrams_tfidf_test
-Y_train = train_DataFrame.author
+#%% Train and predict using unigrams
+train_predict_and_export(
+            unigrams_tfidf, 
+            unigrams_tfidf_test, 
+            train_DataFrame.author, 
+            test_DataFrame.id, 
+            'submission_NaiveBayses.csv')
 
-clf = MultinomialNB().fit(X_train, Y_train)
-prediction = clf.predict_proba(X_test)
+#%% Train and predict using N-grams
 
-classes = Y_train.unique()
-classes.sort()
-
-submission_DF = pd.concat([test_DataFrame.id,
-                           pd.DataFrame(prediction, columns = classes)], 
-                    axis = 1)
-
-submission_DF.to_csv('../output/submission_NaiveBayses.csv', index = False)
+train_predict_and_export(
+        Ngrams_tfidf, 
+        Ngrams_tfidf_test, 
+        train_DataFrame.author, 
+        test_DataFrame, 
+        'submission_NaiveBayses_bigrams')
